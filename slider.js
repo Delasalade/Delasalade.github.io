4ea0453f0a43de6eb31751216df7033f49eb2333
delasalade <76180202+delasalade@users.noreply.github.com>
2022-10-08 22:30:58 +0200

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,17 +1,49 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+const delay = 6000; //ms
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+
+function changeSlide(next = true) {
+    if (next) {
+        current += current > maxLeft ? -100 : current * -1;
+    } else {
+        current = current < 0 ? current + 100 : maxLeft;
+    }
+
+    slides.style.left = current + "%";
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+let autoChange = setInterval(changeSlide, delay);
+const restart = function() {
+    clearInterval(autoChange);
+    autoChange = setInterval(changeSlide, delay);
+};
+
+
+
+
+const more = document.getElementById("more");
+const readmore = document.getElementById("readmore");
+const container = document.getElementById("text-container");
+
+more.addEventListener("click", function toggleread() {
+    readmore.classList.add("visible");
+    more.classList.add("hide");
+
+})
+
+
+const header = document.getElementById("header");
+const phone_ul = document.getElementById("phone-ul");
+//const phone_ul = document.document.getElementsByClassName("phone-menu");
+//const plus = document.getElementById("plus");
+//const plus = document.querySelectorAll("phone");;
+
+plus.addEventListener("click", function opennav() {
+    header.classList.toggle("open");
+    phone_ul.classList.toggle("open");
+    //phone_products.classList.toggle("open");
+});
